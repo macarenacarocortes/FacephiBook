17f32f8e4acbe0678f8eb664645a1756dac6c9da
macarenacarocortes <macarenacaro@facephi.com>
2024-04-24 15:50:58 +0200

    first commit

---
+++ FacephiBook/FacephiBook/Data/FacephiBookContexto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FacephiBook.Data
+{
+    public class FacephiBookContexto : DbContext
+    {
+
+        public FacephiBookContexto(DbContextOptions<FacephiBookContexto> options)
+            : base(options)
+        {
+        }
+
+        /*public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Devolucion> Devoluciones { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Squad> Squads { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // Deshabilitar eliminación en cascada en todas las relaciones
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+      }
+}
---
+++ FacephiBook/FacephiBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FacephiBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
