fe1ecfa31e6fbda96ed7fdfbe5f6428998c2ed80
macarenacarocortes <macarenacaro@facephi.com>
2024-04-25 15:36:03 +0200

    Catalogo - Modelo y controlador

---
+++ FacephiBook/FacephiBook/Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacephiBook.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FacephiBook/FacephiBook/Models/Catalogo.cs
@@ -0,0 +1,16 @@
+﻿namespace FacephiBook.Models
+{
+    public class Catalogo
+    {
+
+        public string? ProductoId { get; set; }
+        public virtual Producto? Producto { get; set; }
+
+        public string? EstadoId { get; set; }
+        public Estado? Estado { get; set; }
+
+        public string? UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+
+    }
+}
