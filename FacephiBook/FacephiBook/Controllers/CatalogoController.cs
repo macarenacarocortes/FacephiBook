ff6eb705ae6a6ea7a49c964195be00a5b6b92e2e
miguelangelferrermirete <miguelangelferrer@facephi.com>
2024-04-26 13:26:01 +0200

    Cambios en Productos controller y en vista

--- FacephiBook/FacephiBook/Controllers/CatalogoController.cs
+++ FacephiBook/FacephiBook/Controllers/CatalogoController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FacephiBook.Data;
+using FacephiBook.Models;
 
 namespace FacephiBook.Controllers
 {
     public class CatalogoController : Controller
     {
-        public IActionResult Index()
+        private readonly FacephiBookContexto _context;
+
+        public CatalogoController(FacephiBookContexto context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var facephiBookContexto = _context.Productos.Include(p => p.Categoria).Include(p => p.Estado).Include(p => p.Reserva);
+            return View(await facephiBookContexto.ToListAsync());
+        }
+
+        // GET: Productos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .Include(p => p.Estado)
+                .Include(p => p.Reserva)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
         }
     }
 }
--- FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
+++ FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62932ab401e6504ce5b5b068696603537284e9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4bf61e8c40390dde2a5ae6fd6ca1fe635795226")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
