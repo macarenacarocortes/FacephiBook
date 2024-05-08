43655d3944d7a4d39f3ce0d197b8542b0393bb42
Ginés Parra Caballero <ginesparra@facephi.com>
2024-05-08 17:08:38 +0200

    .

--- FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
+++ FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b9e71b4e04be1a7727fe1365b83b59c8f50dca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4461b195eeb1f3862b1bc5e7a2656c36934f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
