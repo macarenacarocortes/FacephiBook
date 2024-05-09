9d2f18f1ea1edbc75d458410cfe9a44d48f0b8ef
macarenacarocortes <macarenacaro@facephi.com>
2024-05-09 14:02:01 +0200

    actualizar usuarios, check!

--- FacephiBook/FacephiBook/SeedData.cs
+++ FacephiBook/FacephiBook/SeedData.cs
@@ -12,10 +12,39 @@ namespace FacephiBook
             // Comprobar y crear los roles predeterminados
             var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
             await CrearRolesAsync(roleManager);
+            
             // Comprobar y crear el administrador predeterminado
             var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
             var dbContext = services.GetRequiredService<FacephiBookContexto>();
             await CrearAdminAsync(userManager, dbContext);
+
+
+            // Obtener la lista de usuarios de la tabla local
+            var usuarios = dbContext.Usuarios.ToList();
+
+            // Recorrer la lista de usuarios y agregarlos si no existen en ASP.NET Identity
+            foreach (var usuarioLocal in usuarios)
+            {
+                // Verificar si el usuario ya existe en ASP.NET Identity
+                var usuarioAspNet = await userManager.FindByEmailAsync(usuarioLocal.Email);
+                if (usuarioAspNet == null)
+                {
+                    // El usuario no existe, crearlo en ASP.NET Identity y asignarle su rol
+                    var usuarioNuevo = new IdentityUser
+                    {
+                        UserName = usuarioLocal.Email,
+                        Email = usuarioLocal.Email
+                    };
+                    string usuarioPWD = usuarioLocal.Password;
+
+                    var result = await userManager.CreateAsync(usuarioNuevo, usuarioPWD);
+                    if (result.Succeeded)
+                    {
+                        // Asignar el rol correspondiente
+                        var result1 = await userManager.AddToRoleAsync(usuarioNuevo, usuarioLocal.Rol.ToString());
+                    }
+                }
+            }
         }
         private static async Task CrearRolesAsync(RoleManager<IdentityRole> roleManager)
         {
--- FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
+++ FacephiBook/FacephiBook/obj/Debug/net6.0/FacephiBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9001edb99d00c490b1749dec6cb67092baeb658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162050eeee0a3a48fbf842978fd4a8381098d9c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacephiBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
